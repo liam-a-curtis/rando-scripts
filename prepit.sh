74042c26aa5ce3f9951e5f7de1d6e56d2f02f9a3
Liam Curtis <liam.a.curtis@gmail.com>
2020-06-04 07:05:35 -0400

    more features

--- prepit.sh
+++ prepit.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-#rando script for prepping image files. requires exiftools and argument. use at your own risk
+#rando script for prepping image files. requires exiftools and argument input. use at your own risk
 
 count=1
 DIR=$(pwd)
+
+# error if no argument supplied
+[ $# -eq 0 ] && { echo "Usage: `basename $0` <desired filename>"; exit 1; }
 
 cd "$DIR"
 
@@ -19,7 +22,6 @@ mkdir -p "./resized"
 
 num=1
 for i in `ls *.jpg`; do mv $i $1-$num.jpg; num=$((num+1)); done
-
 for i in `ls *.jpg`; do exiftool -all= -overwrite_original -tagsfromfile @ -exif:Orientation  $i; done
-
+for i in `ls *.jpg`; do convert $i -resize 50% ${i%.*}-m.jpg; echo file copied and resized to 50%; done
 for i in `ls *.jpg`; do convert $i -resize 20% "$DIR"/resized/$i; done
