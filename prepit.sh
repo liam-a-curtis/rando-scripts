16b53d0af758a0700b6cb256557b44040223f0d0
Liam Curtis <liam.a.curtis@gmail.com>
2020-06-04 06:37:23 -0400

    add rename

--- prepit.sh
+++ prepit.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-#rando script for prepping image files. requires exiftools. use at your own risk
+#rando script for prepping image files. requires exiftools and argument. use at your own risk
 
-set -x 
+count=1
 DIR=$(pwd)
 
 cd "$DIR"
 
-rename JPG jpg *.JPG
+if  [ -f *.JPG ];
+  then
+  rename 'y/A-Z/a-z/' *.JPG
+else
+  echo no uppercase jpg files found - good
+fi
+ 
 
-mkdir "./resized"
+mkdir -p "./resized"
+
+num=1
+for i in `ls *.jpg`; do mv $i $1-$num.jpg; num=$((num+1)); done
 
 for i in `ls *.jpg`; do exiftool -all= -overwrite_original -tagsfromfile @ -exif:Orientation  $i; done
 
 for i in `ls *.jpg`; do convert $i -resize 20% "$DIR"/resized/$i; done
-
