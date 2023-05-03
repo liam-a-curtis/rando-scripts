eaf1f7b2457b2ee9ee10bc3d7d4533c1463f5f59
liam-a-curtis <65814899+liam-a-curtis@users.noreply.github.com>
2023-05-03 13:34:58 -0400

    Update nginx-log-checker.sh

--- nginx-log-checker.sh
+++ nginx-log-checker.sh
@@ -4,12 +4,12 @@
 for file in $(ls -1t access.log*); do
   if [[ "${file##*.}" == "gz" ]]; then
     timestamp=$(zcat "$file" | head -n 1 | awk '{print $4, $5}')
-    results=$(zcat "$file" | awk '$9 ~ /^30[1-3]$|^200$/ {print substr($7, 2), $8, $9, $11}' | sort | uniq -c | sort -nr | head)
+    results=$(zcat "$file" | awk '$9 ~ /^30[1-3]$|^200$/ {print $1, substr($7, 2), $9, $11}' | sort | uniq -c | sort -nr)
   else
     timestamp=$(head -n 1 "$file" | awk '{print $4, $5}')
-    results=$(awk '$9 ~ /^30[1-3]$|^200$/ {print substr($7, 2), $8, $9, $11}' "$file" | sort | uniq -c | sort -nr | head)
+    results=$(awk '$9 ~ /^30[1-3]$|^200$/ {print $1, substr($7, 2), $9, $11}' "$file" | sort | uniq -c | sort -nr)
   fi
   echo "=== $file ($timestamp) ==="
-  echo "Count | Requested URL | Response Code | Referrer URL | Redirect Target URL"
+  echo "Count | Timestamp | Requested URL | Response Code | Referrer URL"
   echo "$results"
 done
